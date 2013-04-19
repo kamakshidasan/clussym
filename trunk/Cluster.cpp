4fd6ce273134c874d8ef50abb17673f44b252b80
dilthoms <dilthoms@localhost>
2013-04-20 00:55:30 +0530

    normalize with sqrt(vori*vorj) gives best result for handling meshing inconsistencies

--- trunk/Cluster.cpp
+++ trunk/Cluster.cpp
@@ -22,11 +22,11 @@ void Cluster::GetClusters()
 {
 	for(unsigned int i = 0; i < nPts; i++)
 	{
-		unsigned int sz = kdTree->annkFRSearch(datapts[i], 1.2, 0);
+		unsigned int sz = kdTree->annkFRSearch(datapts[i], 100, 0);
 		printf("For pt %d sphere contains %d\n", i, sz);
 		ANNidxArray nnIdx = new ANNidx[sz];						// allocate near neigh indices
 		ANNdistArray dists = new ANNdist[sz];						// allocate near neighbor dists
-		kdTree->annkFRSearch(datapts[i], 1.2, sz, nnIdx, dists);
+		kdTree->annkFRSearch(datapts[i], 100, sz, nnIdx, dists);
 
 		for (unsigned int j = 0; j < sz; j++) 
 		{
--- trunk/LB.hpp
+++ trunk/LB.hpp
@@ -9,10 +9,11 @@ class LB
 public:
 	void GetEigen(vtkPolyData*, std::vector<std::vector<double> > & surfcords);
 private:
-	double Cotangent(double v1[], double v2[]);
-	void GetCots(vtkIdType *cpts, vtkPoints *pts, double cot[]);
+	double Cotangent(double v1[], double v2[], double & area);
+	void GetVorArea(double cot[3], double len[3], double triarea, double vorarea[3]);
+	void GetCotsLensArea(vtkIdType *cpts, vtkPoints *pts, double cot[], double len[], double & a);
 	bool DuplicateCell(vtkIdType* cpts, boost::unordered_map<Tri, unsigned int> & trimap);
-	bool DuplicateEdge(vtkIdType cpts0, vtkIdType cpts1, float val, boost::unordered_map<Edge, unsigned int> & edmap);
+	bool DuplicateEdge(vtkIdType cpts0, vtkIdType cpts1, boost::unordered_map<Edge, unsigned int> & edmap);
 };
 
 #endif

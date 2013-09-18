4855c107ec39ffbb3e2da1c0e919a6a00f296244
dilthoms <dilthoms@localhost>
2013-09-18 22:38:57 +0530

    adding basic export functionality

--- trunk/BD.hpp
+++ trunk/BD.hpp
@@ -48,6 +48,7 @@ public:
 	SymBranch* BuildSymTree(class ctBranch* b, SymBranch* node, unsigned int & brid, unsigned int & ch, unsigned int & ht);
 	void GetNeighbours(unsigned int k, std::vector<unsigned int> & nbrs, unsigned int ftype);
 	SymBranch* GetBranch(unsigned int bid) { return bridsarr[bid]; };
+	void AppendExportMask(unsigned int, std::vector<unsigned int> & mask);
 	std::vector<int> & GetVertMap();
 	std::vector<Vertex> & m_vlist;
 	std::vector<int> vtobrmap;
